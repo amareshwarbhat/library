46ce014bdc62431b6c345aee444554d64375643b
amareshwarbhat <amareshwarbhat@gmail.com>
2017-03-14 09:35:01 +0530

    gitignore2

--- back-end/models/transactions.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-//Definfing the schema
-module.exports = mongoose.model('Transaction', {
-    userName: String,
-    bookISBN: String,
-    date: String,
-    status: String
-})
